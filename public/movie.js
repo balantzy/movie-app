17695b3a4653383d6da53a039ac583160f44a274
balantzy <balantzy@gmail.com>
2024-12-22 08:21:44 -0500

    commit #5 - finished front-end work and tested functionality

--- public/movie.js
+++ public/movie.js
@@ -10,6 +10,27 @@ const title = document.getElementById("title");
 
 title.textContent = movieTitle;
 
+const div_new = document.createElement('div');
+div_new.innerHTML = `
+    <div class="row">
+    <div class="column">
+    <div class="new_review_card">
+    <h2>Add New Review</h2>
+    <p><strong>Review: </strong>
+    <input type="text" id="new_review" value="">
+    </p>
+    <p><strong>User: </strong>
+    <input type="text" id="new_user" value="">
+    </p>
+    <p><a href="#" onclick="saveReview('new_review', 'new_user')">💾</a>
+    </p>
+    </div>
+    </div>
+    </div>
+`
+
+main.appendChild(div_new)
+
 returnReviews(APILINK);
 
 function returnReviews(url){
@@ -55,4 +76,48 @@ function editReview(id, review, user) {
     <p><a href="#" onclick="saveReview('${reviewInputId}', '${userInputId}', '${id}',)">💾</a>
     </p>
     `
+}
+
+function saveReview(reviewInputId, userInputId, id="") {
+    const review = document.getElementById(reviewInputId).value;
+    const user = document.getElementById(userInputId).value;
+
+    if (id) {
+        fetch(APILINK + id, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "user": user, "review": review })
+        }).then(res => res.json())
+        .then(res => {
+            console.log(res)
+            location.reload();
+        });        
+    } else {
+        fetch(APILINK + "new", {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ "user": user, "review": review, "movieId": movieId})
+        }).then(res => res.json())
+        .then(res => {
+            console.log(res)
+            location.reload();
+        });
+    }
+}
+
+function deleteReview(id) {
+    fetch(APILINK + id, {
+        method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(res => {
+        console.log(res)
+        location.reload();
+    });
 }
